72c78828612e4b812d7d909e9878d50f71b0101a
lucas.simoes <lucas.simoes@matrixsaude.com>
2021-07-18 10:03:13 -0300

    3 - Funcionalidade B

---
+++ GitPractice/FuncionalidadeB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitPractice
+{
+    public static class FuncionalidadeB
+    {
+        public static void DoWork()
+        {
+            Console.WriteLine("DoWork");
+        }
+    }
+}
--- GitPractice/Program.cs
+++ GitPractice/Program.cs
@@ -11,6 +11,7 @@ namespace GitPractice
             Hello3.HelloWorld();
 
             FuncionalidadeA.DoWork();
+            FuncionalidadeB.DoWork();
         }
     }
 }
